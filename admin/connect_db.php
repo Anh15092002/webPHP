5b15ed3774f1d5478e7c43cb8cd9c27b5618ca25
Anh15092002 <trantuananh0220@gmail.com>
2023-05-06 20:38:02 +0700

    upload file giỏ hàng

--- admin/connect_db.php
+++ admin/connect_db.php
@@ -2,10 +2,10 @@
 
 $host = "127.0.0.1";
 $user = "root";
-$password = "root";
-$database = "giaythethao";
+$password = "";
+$database = "giaythethao2";
 $con = mysqli_connect($host, $user, $password, $database);
 if (mysqli_connect_errno()) {
     echo "Connection Fail: " . mysqli_connect_errno();
-    exit;
+    exit; 
 }
